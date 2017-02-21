f0dd2ec4654307f4ef4eedebb57961c06f83ee56
Ales Rechtorik <ales@f13.cz>
2017-02-21 14:39:13 +0100

    add async example

--- src/demo-app/select/select-demo.ts
+++ src/demo-app/select/select-demo.ts
@@ -1,6 +1,14 @@
 import {Component} from '@angular/core';
-import {FormControl} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 import {MdSelectChange} from '@angular/material';
+import {Http} from '@angular/http';
+
+import 'rxjs/add/operator/debounce';
+import 'rxjs/add/operator/withLatestFrom';
+import 'rxjs/add/observable/timer';
+import {Observable} from 'rxjs/Observable';
+
+const GITHUB_API_ENDPOINT = 'https://api.github.com';
 
 @Component({
     moduleId: module.id,
@@ -16,6 +24,29 @@ export class SelectDemo {
   searchTerm: string;
   latestChangeEvent: MdSelectChange;
   foodControl = new FormControl('pizza-1');
+
+  selectHeaderAsyncControl = new FormGroup({
+    search: new FormControl(),
+    selected: new FormControl({})
+  });
+
+  users$ = this.selectHeaderAsyncControl
+        .get('search')
+        .valueChanges
+        .startWith(null)
+        .debounce(() => Observable.timer(200))
+        .switchMap((term) => {
+          if (term) {
+            return this._http
+              .get(`${GITHUB_API_ENDPOINT}/search/users?q=${term}`)
+              .map((response) => response.json())
+              .map((data) => data.items);
+          } else {
+            return this._http
+              .get(`${GITHUB_API_ENDPOINT}/users`)
+              .map((response) => response.json());
+          }
+        });
 
   foods = [
     {value: 'steak-0', viewValue: 'Steak'},
@@ -42,6 +73,8 @@ export class SelectDemo {
     {value: 'charizard-1', viewValue: 'Charizard'},
     {value: 'squirtle-2', viewValue: 'Squirtle'}
   ];
+
+  constructor(private _http: Http) { }
 
   toggleDisabled() {
     this.foodControl.enabled ? this.foodControl.disable() : this.foodControl.enable();
